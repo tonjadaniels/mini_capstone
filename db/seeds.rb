81f34e91d1cdaf75ed618cd9bebba68ef54e738e
Tonja <tonja64@gmail.com>
2018-05-29 15:19:33 -0700

    added database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Product.create({name: "Design Essentials Honey Curl Forming Custard", price: 8, description: "Styling custard that holds you curl shape! Your gonna love your hair", image_url: "https://www.amazon.com/Design-Essentials-Natural-Chamomile-Definition-8oz/dp/B00FQ0QVDY/ref=sr_1_6_a_it/135-6458950-8705221?ie=UTF8&qid=1527631576&sr=8-6&keywords=design+essentials+hair+products"})
+
+Product.create({name: "Design Essentials Almond Avocado Shampoo", price: 8, description: "Makes shampooing easy.", image_url: "https://www.amazon.com/Design-Essentials-Moisturizing-Detangling-Collection/dp/B00U45Z1P0/ref=sr_1_3_a_it/135-6458950-8705221?ie=UTF8&qid=1527631576&sr=8-3&keywords=design+essentials+hair+products"})
+
+Product.create({name: "Design Essentials Almond Avocado Leave in", price: 8, description: "conditioning hair.", image_url: "https://www.amazon.com/Design-Essentials-Conditioner-Moisturized-Hair-Almond/dp/B00FQ0GU3K/ref=sr_1_8_a_it/135-6458950-8705221?ie=UTF8&qid=1527631576&sr=8-8&keywords=design+essentials+hair+products"})
